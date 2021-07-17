9214d526a5f513a100a93be600d64d400f01cb7d
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-07-17 10:25:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const clockText = () => {
         textMinutes.textContent = mm;
 
         //mostrar la hora
-        textHour.textContent = `${hh}`;
+        textHour.textContent = `${hh}:`;
 
         //mostrar am o pm
         textAMPM.innerHTML = ampm
@@ -67,4 +67,35 @@ const clockText = () => {
         dateMonth.textContent = `${months[month]},`;
         dateYear.textContent = year;
 }
+
 setInterval(clockText, 1000)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-icon')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
